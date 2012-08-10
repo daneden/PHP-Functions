784b7ea0ee423f5e97a225d59847e10e08061097
jkoech <kippontherun@gmail.com>
2012-08-10 11:22:16 +0300

    Update functions.php
    Added GPC hack

--- functions.php
+++ functions.php
@@ -64,4 +64,29 @@ function show_gravatar($email, $size, $default, $rating) { /* allows use of grav
 }
 
 
+/*Useful magic_quotes_gpc hack*/
+
+if (get_magic_quotes_gpc())
+{
+  $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+  while (list($key, $val) = each($process))
+  {
+    foreach ($val as $k => $v)
+    {
+      unset($process[$key][$k]);
+      if (is_array($v))
+      {
+        $process[$key][stripslashes($k)] = $v;
+        $process[] = &$process[$key][stripslashes($k)];
+      }
+      else
+      {
+        $process[$key][stripslashes($k)] = stripslashes($v);
+      }
+    }
+  }
+  unset($process);
+}
+
+
 ?>
